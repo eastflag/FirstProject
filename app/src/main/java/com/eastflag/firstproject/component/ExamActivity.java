aa3d4b505a3cd263aea3df82d284b60eb47e7204
ace001 <jeteaime012@naver.com>
2016-04-02 15:58:24 +0900

    4주차 - 주소록 호출

--- app/src/main/java/com/eastflag/firstproject/component/ExamActivity.java
+++ app/src/main/java/com/eastflag/firstproject/component/ExamActivity.java
@@ -1,24 +1,29 @@
 package com.eastflag.firstproject.component;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.eastflag.firstproject.R;
 
 public class ExamActivity extends Activity {
 
     public static final int GET_GALLERY = 100;
+    public static final int GET_CONTACT = 200;
     private ImageView imgPicture;
+    private TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,7 @@ public class ExamActivity extends Activity {
         setContentView(R.layout.activity_exam);
 
         imgPicture = (ImageView) findViewById(R.id.imageView);
+        tvDisplay = (TextView) findViewById(R.id.textView);
 
         findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +43,17 @@ public class ExamActivity extends Activity {
                 startActivityForResult(intent, GET_GALLERY);
             }
         });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 주소록 호출
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_PICK);
+                intent.setData(ContactsContract.Contacts.CONTENT_URI);
+                intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+                startActivityForResult(intent, GET_CONTACT);
+            }
+        });
     }
 
     @Override
@@ -46,22 +63,39 @@ public class ExamActivity extends Activity {
                 case GET_GALLERY:
                     // 주소록 데이터 처리
                     //Content Provider를 이용해서 MediaDB 사진 데이터의 실제 path 가져오기
-                    Uri selectedImage = data.getData();
-                    Log.d("LDK", selectedImage.toString());
-                     //content://media/external/images/media/24143
-                     String[] filePathColumn = { MediaStore.Images.Media.DATA };
-                     //이미지화일의 실제 path 컬럼
-                     Cursor cursor = getContentResolver().query(selectedImage,
-                             filePathColumn, null, null, null);
-                    cursor.moveToFirst();
-                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                    if(columnIndex < 0) // no column index
-                        return; // DO YOUR ERROR HANDLING
-                    String picturePath = cursor.getString(columnIndex);
-                    Log.d("LDK", "file path:" + picturePath);
-                    //실제 경로에 접근해서 사진데이터를 가져와서 imageView에 뿌리기
-                    Bitmap mBitmap = BitmapFactory.decodeFile(picturePath);
-                    imgPicture.setImageBitmap(mBitmap);
+                    try {
+                        Uri selectedImage = data.getData();
+                        Log.d("LDK", selectedImage.toString());
+                        //content://media/external/images/media/24143
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        //이미지화일의 실제 path 컬럼
+                        Cursor cursor = getContentResolver().query(selectedImage,
+                                filePathColumn, null, null, null);
+                        cursor.moveToFirst();
+                        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                        if (columnIndex < 0) // no column index
+                            return; // DO YOUR ERROR HANDLING
+                        String picturePath = cursor.getString(columnIndex);
+                        Log.d("LDK", "file path:" + picturePath);
+                        //실제 경로에 접근해서 사진데이터를 가져와서 imageView에 뿌리기
+                        Bitmap mBitmap = BitmapFactory.decodeFile(picturePath);
+                        //샘플링 or 압축
+                        imgPicture.setImageBitmap(mBitmap);
+                    } catch(Exception e) {
+                        Log.d("LDK", "error:" + e.getMessage());
+                    }
+                    break;
+                case GET_CONTACT:
+                    ContentResolver cr = getContentResolver();
+                    Cursor phoneCur = cr.query(data.getData(), new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER,
+                            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME},null, null, null);
+                    while (phoneCur.moveToNext()) {
+                        String phone = phoneCur.getString(0); //전화번호
+                        String name = phoneCur.getString(1); //이름
+                        tvDisplay.setText("phone:" + phone);
+                        tvDisplay.append(" name:"+ name);
+                    }
+                    phoneCur.close();
                     break;
             }
         }
